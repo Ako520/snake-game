2b1318c8af3c7b000417a8ff3d7b89d4aa22e43a
Ako520 <jingnanbendan@163.com>
2018-12-06 13:27:43 +0800

    使用styled-components主题代替颜色变量

---
+++ src/app/styles/theme/mainTheme.js
@@ -0,0 +1,47 @@
+const mainTheme = {
+  red    :'#f00',
+  blue   :'#00f',
+  white  :'#fff',
+  gray   :'#999',
+  black  :'#000',
+  lightText: '#999999', //产品列表 title颜色
+  darkText: '#666666', //产品列表 content颜色
+  text: '#333333', //Form placeholder颜色
+  placrholderText: '#bbbbbb', //Button 紫色的Text
+  purpleBText: '#7d5ae7', //Button【使用优惠码】 紫色的Text
+  blueText: 'rgb(40,108,172)', //单选 未选中 text颜色
+  cknoSelect: '#8d79f5', //单选 已选中 text颜色
+  ckSelect: '#8d79f5', //我的 红色1份
+  redText: '#ff6363',
+  grayText: 'rgb(204, 204, 204)',
+  disabledButton: '#bbbbbb', //Button 紫色的Text
+  tableHead: 'rgb(136, 136, 136)', //table-head的text颜色
+  warning: 'rgb(250, 91, 69)', //end-time
+  loginInputLine: 'rgb(187,187,187)', //login input bottom线
+  darkLine: 'rgb(234,234,234)', //注册的线
+  line: 'rgb(238,238,238)', //header线
+  inputline: '#dddddd',
+  textareaLine: '#e4e4e4',
+  purpleLine: 'rgb(141,121,245)',
+  blueLine: 'rgb(40, 108, 172)', //upload-out dragger
+  grayLine: 'rgb(237, 237, 237)', //modal-table
+  blueSelectLine: 'rgb(73,169,238)',
+  gradientOne: '#ff6eac', //渐变从左到右 最后一个颜色
+  gradient: '#a269f0',
+  bgLightGary: '#f9f9f9', //背景色 灰色
+  bgpaleLavender: 'rgb(251, 249, 255)',
+  bgGray: 'rgb(247, 247, 247)', //报告form表单背景 upload-out table-head背景
+  bgUploadAbleOut: 'rgb(246, 246, 255)', //报告form表单背景 upload-out-able
+  bgUploadAbleIn: 'rgb(250, 249, 255)', //报告form表单背景 upload-in-able
+  bgBlueHover: 'rgb(24, 99, 170)',
+  bgWarningTipBorder: 'rgb(254, 227, 224)', // warning-tip
+  bgWaringTip: 'rgb(255, 243, 242)',
+  bgBlue: 'rgb(40,108,172)',  // 深蓝色button
+  bgPurpleOpacity:'rgba(125, 90, 230, 0.40)',
+  bgBlueLightHover:'rgb(120,188,252)', // input 下拉菜单
+  bgBlueSelect:'rgb(73,169,238)',
+  boxShadowInput:'0 0 0 2px rgba(40,108,172, 0.2)',
+
+}
+
+export default mainTheme
