c803b7baa83a2b63794d0cb3bba436f087e77a0d
Ako520 <jingnanbendan@163.com>
2018-12-06 16:44:41 +0800

    add control store

---
+++ src/app/stores/control.js
@@ -0,0 +1,56 @@
+// @flow
+
+import {DIRECTION, COMMAND, GAMESTATUS} from '../utils/const'
+import { observable } from 'mobx'
+import ground from './ground'
+
+class Control {
+  @observable gameStatus = GAMESTATUS.WAITING
+  ground: ground
+
+  constructor() {
+    this.bindKeyDownEvent()
+  }
+
+  bindKeyDownEvent = () => {
+    const { BOTTOM, RIGHT, TOP, LEFT } = DIRECTION
+    const directionKeyCode = [BOTTOM, RIGHT, TOP, LEFT]
+
+    window.document.onkeydown = (e) => {
+      if (directionKeyCode.indexOf(e.keyCode) !== -1) {
+        this.ground.snake.turnDirection(e.keyCode)
+      }
+      console.log('e.keyCode', e.keyCode)
+      switch (e.keyCode) {
+        case BOTTOM || RIGHT || TOP || LEFT:
+          this.ground.snake.turnDirection(e.keyCode)
+          break;
+        case COMMAND.TOGGLEGAME:
+          this.startOrPauseGame()
+          break;
+        default:
+
+      }
+    }
+  }
+
+  startOrPauseGame = () => {
+    const startGame = () => {
+      this.gameStatus = GAMESTATUS.PLAYING
+      this.ground.snake.start()
+    }
+
+    const pauseGame = () => {
+      this.gameStatus = GAMESTATUS.WAITING
+      this.ground.snake.pause()
+    }
+
+    if (this.gameStatus === GAMESTATUS.WAITING) {
+      startGame()
+    } else {
+      pauseGame()
+    }
+  }
+}
+
+export default new Control()
