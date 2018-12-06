5a3b99554189d4c81570b9db0e9d542f4e7fea18
Ako520 <jingnanbendan@163.com>
2018-12-06 16:46:50 +0800

    add const.js to manage const variable

---
+++ src/app/utils/const.js
@@ -0,0 +1,23 @@
+// @flow
+const DIRECTION = {
+  BOTTOM: 40,
+  RIGHT: 39,
+  TOP: 38,
+  LEFT: 37,
+}
+
+const GAMESTATUS = {
+  PLAYING: 'playing',
+  GAMEOVER: 'gameover',
+  WAITING: 'waiting'
+}
+
+const COMMAND = {
+  TOGGLEGAME: 32
+}
+
+export {
+  DIRECTION,
+  COMMAND,
+  GAMESTATUS,
+}
