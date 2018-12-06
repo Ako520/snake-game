0384659a40d842ac216b9333ff193c6b14a0e83c
Ako520 <jingnanbendan@163.com>
2018-12-06 13:28:00 +0800

    应用SC主题

--- src/index.js
+++ src/index.js
@@ -5,24 +5,14 @@ import ReactDOM from "react-dom"
 import App from './App.js'
 import { Provider } from 'mobx-react'
 import stores from './app/stores/index'
+import { ThemeProvider } from 'styled-components'
+import mainTheme from './app/styles/theme/mainTheme'
+
 ReactDOM.render(
-  <Provider {...stores}>
-    <App />
-  </Provider>,
+  <ThemeProvider theme={mainTheme}>
+    <Provider {...stores}>
+      <App />
+    </Provider>
+  </ThemeProvider>,
   document.getElementById("root")
 )
-
-
-
-// import React from "react"
-// import ReactDOM from "react-dom"
-// import { Provider } from "mobx-react"
-// import stores from "app/stores"
-// import App from "app"
-//
-// ReactDOM.render(
-//   <Provider {...stores}>
-//     <App />
-//   </Provider>,
-//   document.getElementById("root")
-// )
